18d58b6c4a404604026229bc13c662beed975f22
Enzo Damascena <enzodm.dev@gmail.com>
2025-03-23 21:52:15 -0300

    feat: update images, add therapy section, and implement WhatsApp contact button; enhance layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
+import Therapy from "@/components/modules/Therapy";
 import Start from "../components/modules/Start";
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main>
       <Start />
-
-      <section id="about" className="h-screen flex items-center justify-center bg-gray-100">
-        <h2 className="text-4xl font-bold">About Us</h2>
-      </section>
+      <Therapy />
 
       <section id="services" className="h-screen flex items-center justify-center bg-gray-100">
         <h2 className="text-4xl font-bold">Our Services</h2>
--- components/modules/Start.tsx
+++ components/modules/Start.tsx
@@ -1,24 +1,55 @@
+import { sections } from "@/constants/sections";
 import Image from "next/image";
+import { aboutParagraphs } from "../../constants/aboutParagraphs";
 import { name } from "../../constants/name";
+import { whatsAppUrl } from "../../constants/whatsAppUrl";
 
 export default function Start() {
     return (
-      <section id="home" className="flex flex-row">
+      <section id={sections[0].id} className="flex flex-col justify-center lg:flex-row p-4 space-x-32 space-y-4 bg-[var(--start-section-bg)]">
         <Image
-            src="/photo-1.jpeg"
+            src="/photo-3.jpeg"
             alt="Description of the image"
-            width={0} 
+            width={0}
             height={0} 
-            sizes="33vw"
-            className="w-1/3 h-auto"
+            className="w-full max-w-lg rounded-md"
             unoptimized
             priority
             quality={100}
         />
-        <div>
-          <h1 className="app-name">{name}</h1>
-            <p className="text-lg">Transforme suas emoções, redescubra sua força.</p>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">Saiba mais</button>
+        <div className="flex flex-col space-y-3 justify-center w-full max-w-sm lg:max-w-md xl:max-w-lg">
+          <div className="flex flex-col space-y-3">
+            <h1 className="app-name !text-5xl md:!text-6xl">{name}</h1>
+            <h2>Terapeuta Emocional</h2>
+          </div>
+          <div className="flex flex-col space-y-3">
+            <p>{aboutParagraphs.first}</p>
+            <p>{aboutParagraphs.second}</p>
+            <p>{aboutParagraphs.third}</p>
+          </div>
+          <a
+            href={whatsAppUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center self-start gap-2 mt-4 !bg-[var(--font-app-color)] !text-white px-6 py-2 rounded-md hover:!bg-opacity-50"
+            aria-label="Fale comigo no WhatsApp"
+          >
+            Fale comigo aqui
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </a>
         </div>
       </section>
     );
